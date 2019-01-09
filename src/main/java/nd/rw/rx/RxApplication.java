ab83b8a78427cab0afe40f2a1ec2453da9d02fb5
Andrzej Torski (ato05) <andrzej.torski@hrs.com>
2019-01-09 20:23:50 +0100

    init

---
+++ src/main/java/nd/rw/rx/RxApplication.java
@@ -0,0 +1,14 @@
+package nd.rw.rx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxApplication.class, args);
+    }
+
+}
+
